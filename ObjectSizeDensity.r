7fc159abf4cbe9ea4c386957e32192d7ad15f6f6
andruby <andrew@bedesign.be>
2010-06-02 17:44:13 +0200

    need the mains

--- ObjectSizeDensity.r
+++ ObjectSizeDensity.r
@@ -2,7 +2,7 @@
 scriptname = "ObjectSizeDensity"
 source("include.r")
 
-par(mar=c(4,4,1,1), cex=2)
+par(mar=c(4,4,1,2), cex=2)
 
 ## Get The Data
 between_time = "time between '2009-02-22 00:00:00' and '2009-02-28 23:59:59'"
@@ -13,7 +13,7 @@ data = dbGetQuery(con, paste("select avg(size) AS 'size' from ",db," where size 
 
 ## Logaritmic Density Plot
 cat("Plotting Chart \n")
-plot(density(log10(data_all$size)),col="red",xlim=c(2,8),ylim=c(0,2),xlab="Tamaño de Objeto",ylab="Densidad (%)",axes=FALSE)
+plot(density(log10(data_all$size)),main="",col="red",xlim=c(2,8),ylim=c(0,2),xlab="Tamaño de Objeto",ylab="Densidad (%)",axes=FALSE)
 axis(1,at=c(2,3,4,5,6,7,8),labels=c("100B","1KB","10KB","100KB","1MB","10MB","100MB"))
 axis(2)
 lines(density(log10(data$size)),col="blue", lty=2)

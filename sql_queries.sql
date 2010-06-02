c995026ee3c8a029e5a359c1245d2c4e3974fb68
andruby <andrew@bedesign.be>
2010-06-02 21:37:31 +0200

    ADd query

--- sql_queries.sql
+++ sql_queries.sql
@@ -4,3 +4,5 @@ select method,count(*),sum(size) from full group by method;
 -- Days of Week
 select DAYOFWEEK(time),count(*),sum(size) from small group by DAYOFWEEK(time);
 
+-- Build the small table from the full table
+insert into small select * from full where rand < 0.01;

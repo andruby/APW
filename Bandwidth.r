6b30bf0b557f4aa55df1bc9c7f7e07db8c2825e8
andruby <andrew@bedesign.be>
2010-06-02 16:03:50 +0200

    per hour slots

--- Bandwidth.r
+++ Bandwidth.r
@@ -8,7 +8,7 @@ source("include.r")
 par(mar=c(4, 4, 1, 2),cex=1.8)
 
 ## Sequential timeslots per 20 minutes since the start of the year
-mysql_year_seconds="((TIME_TO_SEC(time) + TO_DAYS(time) * (24*60*60)) div 1200)"
+mysql_year_seconds="((TIME_TO_SEC(time) + TO_DAYS(time) * (24*60*60)) div 3600)"
 
 ## Get The Data
 cat("Getting Data (NON HIT BW) \n")
@@ -18,17 +18,17 @@ bw_total = dbGetQuery(con, paste("SELECT ",mysql_year_seconds," AS 'slot',sum(si
 
 cat("Calculating X en Y's \n")
 min_bw_x = min(bw_total$slot)
-bw_x = ((bw_total$slot-min_bw_x) / (24*3))
-# turn bytes per 20 minutes into Megabits per seconds
-bw_total = (bw_total$size * 8) / (1200 * 1024 * 1024)
-bw_nonhit = (bw_nonhit$size * 8) / (1200 * 1024 * 1024)
+bw_x = ((bw_total$slot-min_bw_x) / (24))
+# turn bytes per hour into Megabits per seconds
+bw_total = (bw_total$size * 8) / (3600 * 1024 * 1024)
+bw_nonhit = (bw_nonhit$size * 8) / (3600 * 1024 * 1024)
 bw_diff = bw_total-bw_nonhit
 
 max_y = max(bw_total)
 
 cat("Plotting Chart \n")
 plot(bw_x,bw_total,xlab="Days",ylab="Ancho de banda (Mbps)", axes=F, col="blue", type="l", ylim=c(0,max_y))
-#axis(1, at=c(0:12)*2, labels=c(0:12)*2)
+axis(1, at=c(0:8)*2, labels=c(0:8)*2)
 axis(1)
 axis(2, las=2)
 box()
@@ -36,6 +36,6 @@ lines(bw_x,bw_total,col="blue",lwd=1)
 #lines(bw_x,bw_nonhit,col="red",lwd=2)
 lines(bw_x,bw_diff,col="green",lwd=2)
 
-legend(1,max_y*0.9,legend=c("Total","Diff"), col=c("blue","green"), lwd=c(1,2), bty="n")
+legend(0.5,max_y*0.95,legend=c("Total","Diff"), col=c("blue","green"), lwd=c(1,2))
 
 source("bottom.r")

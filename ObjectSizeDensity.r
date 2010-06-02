e1fcc0da8999eaf4b4564212525aef28c4df9546
andruby <andrew@bedesign.be>
2010-06-02 21:31:37 +0200

    Basic Data Cache for quicker rebuilding of charts

--- ObjectSizeDensity.r
+++ ObjectSizeDensity.r
@@ -5,18 +5,26 @@ source("include.r")
 par(mar=c(4,4,1,2), cex=2)
 
 ## Get The Data
-between_time = "time between '2009-02-22 00:00:00' and '2009-02-28 23:59:59'"
-cat("Getting Data (Requests) \n")
-data_all = dbGetQuery(con, paste("select size AS 'size' from ",db," where size > 1 and ",between_time))
-cat("Getting Data (Uniques) \n")
-data = dbGetQuery(con, paste("select avg(size) AS 'size' from ",db," where size > 1 and ",between_time," group by MD5(uri)"))
+#d1_fn <- paste("data_",scriptname,".gz",sep="")
+if(!read_cache()) {
+	between_time = "time between '2009-02-22 00:00:00' and '2009-02-28 23:59:59'"
+	cat("Getting Data (Requests) \n")
+	data_all = dbGetQuery(con, paste("select size AS 'size' from ",db," where size > 1 and ",between_time))
+	cat("Getting Data (Uniques) \n")
+	data = dbGetQuery(con, paste("select avg(size) AS 'size' from ",db," where size > 1 and ",between_time," group by MD5(uri)"))
+
+	## save data to file
+	d1 <- density(log10(data_all$size))
+	d2 <- density(log10(data$size))
+	write_cache(c("d1","d2"))
+}
 
 ## Logaritmic Density Plot
 cat("Plotting Chart \n")
-plot(density(log10(data_all$size)),main="",col="red",xlim=c(2,8),xlab="Tamaño de Objeto",ylab="Densidad (%)",axes=FALSE)
+plot(d1,main="",col="red",xlim=c(2,8),xlab="Tamaño de Objeto",ylab="Densidad (%)",axes=FALSE)
 axis(1,at=c(2,3,4,5,6,7,8),labels=c("100B","1KB","10KB","100KB","1MB","10MB","100MB"))
 axis(2)
-lines(density(log10(data$size)),col="blue", lty=2)
+lines(d2,col="blue", lty=2)
 legend(6,2,legend=c("Pedidos", "Únicos"), col=c("red","blue"), lwd=1:1, lty=1:2, bty="n")
 
 source("bottom.r")
--- include.r
+++ include.r
@@ -8,6 +8,25 @@ if(exists("db")) {
   con <- dbConnect(MySQL(), user="apw", password="apw", dbname="apw", host=hostname)
 }
 
+## cache functions
+read_cache <- function() {
+	fn <- paste("data_",scriptname,".gz",sep="")
+ 	if(file.exists(fn)) {
+		cat("Loading Data from File",fn,"\n")
+		load(fn)
+		return(TRUE)
+	} else {
+  		return(FALSE)
+	}
+}
+
+write_cache <- function(list) {
+	fn <- paste("data_",scriptname,".gz",sep="")
+	cat("Saving Data To File",fn,"\n")
+ 	save(list=list,file=fn,compress="gzip")
+	cat(system(paste("ls -lh",fn,"\n"),intern=T))
+}
+
 ## output device
 if(exists("output_file") == F) {
   output_file = paste("../charts/",scriptname,"_",db,".eps",sep="")

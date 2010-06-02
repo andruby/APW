047bc0c282551aed7e8202994b78e92880bcc656
andruby <andrew@bedesign.be>
2010-06-02 16:12:25 +0200

    typos

--- Bandwidth.r
+++ Bandwidth.r
@@ -24,18 +24,19 @@ bw_total = (bw_total$size * 8) / (3600 * 1024 * 1024)
 bw_nonhit = (bw_nonhit$size * 8) / (3600 * 1024 * 1024)
 bw_diff = bw_total-bw_nonhit
 
-max_y = max(bw_total)
+#max_y = max(bw_total)
+max_y = 50
 
 cat("Plotting Chart \n")
 plot(bw_x,bw_total,xlab="Dias",ylab="Ancho de banda (Mbps)", axes=F, col="blue", type="l", ylim=c(0,max_y))
 axis(1, at=c(0:8)*2, labels=c(0:8)*2)
-axis(1)
+#axis(1)
 axis(2, las=2)
 box()
 lines(bw_x,bw_total,col="blue",lwd=1)
 #lines(bw_x,bw_nonhit,col="red",lwd=2)
 lines(bw_x,bw_diff,col="green",lwd=2)
 
-legend(0.5,max_y*0.95,legend=c("Total","Ahorrado"), col=c("blue","green"), lwd=c(1,2))
+legend(2,max_y*0.95,legend=c("Total","Ahorrado"), col=c("blue","green"), lwd=c(1,2), bty="n")
 
 source("bottom.r")

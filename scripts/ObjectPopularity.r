4a5ece033ca2e8830ad1a3ea08ad253244937c2a
andruby <andrew@bedesign.be>
2010-06-09 03:29:06 +0200

    Use newer db format

--- scripts/ObjectPopularity.r
+++ scripts/ObjectPopularity.r
@@ -6,17 +6,19 @@ source("include.r")
 
 ## params
 par(mar=c(4,4,1,2),cex=2)
-sample_size = 10000
-top_percent=0.02
+sample_size = 1000
+top_percent=0.001
 
 ## Get The Data
 if(!read_cache()) {
 	cat("Getting Data \n")
-	tod = dbGetQuery(con, paste("SELECT MD5(uri),count(*) AS 'count' from ",db,
-								" where method = 'GET' group by MD5(uri)"))
-	# only take the top {top_percent}%
+	tod = dbGetQuery(con, paste("SELECT uri_id,count(*) AS 'count' from ",db,
+								" where method = 'GET' group by uri_id"))
+	
+	cat("Filtering for top ",top_percent,"% \n")
 	amount = length(tod$count)
 	data <- sort(tod$count, decreasing = T)[1:(amount*top_percent)]
+	
 	cat("Sampling \n")
 	data <- c(max(data),sort(sample(data, size = sample_size), decreasing = T),min(data))
 	write_cache(c("data"))
